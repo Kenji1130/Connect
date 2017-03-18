dbe3e59555554f76c8826bf35b3df843c12bc242
Kenji1130 <kenjikimura@mail.com>
2017-03-17 17:56:52 -0700

    Add Firebase Auth, Login, Signup Mode.

---
+++ Connect/CNOnboardingSignTypeVC.h
@@ -0,0 +1,13 @@
+//
+//  CNOnboardingSignTypeVC.h
+//  Connect
+//
+//  Created by mac on 3/17/17.
+//  Copyright © 2017 Connect Social Network. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CNOnboardingSignTypeVC : UIViewController
+
+@end
